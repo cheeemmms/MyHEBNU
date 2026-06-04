860954a7c3fd2ebae3dd515519242a4ded5865f4
cheeemmms <cheeemmms@users.noreply.github.com>
2026-06-04 23:14:27 +0800

    Add Aliyun mirrors for faster dependency downloads in China

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,8 @@
 pluginManagement {
     repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
         google()
         mavenCentral()
         gradlePluginPortal()
@@ -10,6 +13,8 @@ dependencyResolutionManagement {
     @Suppress("UnstableApiUsage")
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
         google()
         mavenCentral()
     }

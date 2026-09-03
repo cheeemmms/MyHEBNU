e3de510114ef20400cb8778f2f4820ec9b620175
cheeemmms <cheeemmms@users.noreply.github.com>
2026-09-04 00:22:07 +0800

    chore(release): 版本升至 1.4.0 并更新 CHANGELOG

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.myhebnu"
         minSdk = 31
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.3.1"
+        versionCode = 6
+        versionName = "1.4.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
